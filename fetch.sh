64749f147a4a23c999b2f0c525063c198c55a082
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-19 20:38:58 -0600

    converted ram kb to mb; simplified os

--- fetch.sh
+++ fetch.sh
@@ -18,14 +18,14 @@ exec 2>/dev/null
 echo -ne "${RED}host${NC} ~ " ; uname -n
 echo -ne "${YELLOW}arch${NC} ~ " ; uname -m
 echo -ne "${GREEN}kernel${NC} ~ " ; uname -r
-# uptime w/ uptime
+# uptime using uptime
 echo -ne "${CYAN}uptime${NC} ~ " ; uptime --pretty| sed -e 's/up//'
-# shell from enviornment variable
+# check shell enviornment variable
 echo -ne "${BLUE}shell${NC} ~ " ; echo $SHELL|sed 's/usr.*bin//g' | tr -d '/'
-# RETTY_NAME from /etc/os-release
-echo -ne "${PURPLE}os${NC} ~ " ; grep 'PRETTY' /etc/os-release | sed 's/PRETTY_NAME=//g' | tr -d '"'
-# active desktop enviornment with wmctrl
-echo -ne "${RED}de${NC} ~ " ; wmctrl -m | grep 'Name:' | sed 's/Name: //g'
+# PRETTY_NAME from /etc/os-release
+echo -ne "${PURPLE}os${NC} ~ " ; awk -F '"' '/PRETTY/ {print $2}' /etc/os-release
+# active desktop enviornment with wmctrl \\ !rmv dependacy to external package
+echo -ne "${RED}de/wm${NC} ~ " ; wmctrl -m | grep 'Name:' | sed 's/Name: //g'
 # gtk theme, if exist print name
 echo -ne "${YELLOW}gtk${NC} ~ " ; grep 'gtk-theme-name' ~/.config/gtk-3.0/* | sed 's/gtk-theme-name=//g' | sed 's/-/ /g'
 # cpu with lscpu
@@ -33,6 +33,6 @@ echo -ne "${GREEN}cpu${NC} ~ " ; lscpu | grep 'Model\ name' | sed 's/^[Model nam
 # installed packages from package manager
 echo -ne "${CYAN}pkgs${NC} ~ " ; pacman -Q | wc -l
 # MemTotal in /proc/meminfo
-echo -ne "${BLUE}ram${NC} ~ " ; grep 'MemTotal:' /proc/meminfo | sed 's/MemTotal://g' | sed 's/^[ \t]*//' 
+echo -ne "${BLUE}ram${NC} ~ " ; awk '/MemTotal:/ {printf "%d MiB\n", $2 / 1024}' /proc/meminfo 
 # TERM info from printenv
 echo -ne "${PURPLE}term${NC} ~ " ; printenv | grep -w 'TERM' | sed 's/TERM=//g'
