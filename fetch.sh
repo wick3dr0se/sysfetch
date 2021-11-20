4d88a6ea08d0c45a85ca4bd8b0b2f41a144920a3
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-20 00:39:16 -0600

    drop lscpu

--- fetch.sh
+++ fetch.sh
@@ -18,21 +18,30 @@ exec 2>/dev/null
 echo -ne "${RED}host${NC} ~ " ; uname -n
 echo -ne "${YELLOW}arch${NC} ~ " ; uname -m
 echo -ne "${GREEN}kernel${NC} ~ " ; uname -r
+
 # uptime using uptime
-echo -ne "${CYAN}uptime${NC} ~ " ; uptime --pretty| sed -e 's/up//'
+echo -ne "${CYAN}uptime${NC} ~ " ; uptime --pretty | sed -e 's/up//'
+
 # check shell enviornment variable
-echo -ne "${BLUE}shell${NC} ~ " ; echo $SHELL|sed 's/usr.*bin//g' | tr -d '/'
+echo -ne "${BLUE}shell${NC} ~ " ; echo $SHELL | sed 's%.*/%%'
+
 # PRETTY_NAME from /etc/os-release
 echo -ne "${PURPLE}os${NC} ~ " ; awk -F '"' '/PRETTY/ {print $2}' /etc/os-release
-# active desktop enviornment with wmctrl \\ !rmv dependacy to external package
+
+# active desktop enviornment with wmctrl \\ !remove dependacy to external package
 echo -ne "${RED}de/wm${NC} ~ " ; wmctrl -m | grep 'Name:' | sed 's/Name: //g'
+
 # gtk theme, if exist print name
 echo -ne "${YELLOW}gtk${NC} ~ " ; grep 'gtk-theme-name' ~/.config/gtk-3.0/* | sed 's/gtk-theme-name=//g' | sed 's/-/ /g'
-# cpu with lscpu
-echo -ne "${GREEN}cpu${NC} ~ " ; lscpu | grep 'Model\ name' | sed 's/^[Model name:.* \t]*//'
+
+# print model name from /proc/cpuinfo
+echo -ne "${GREEN}cpu${NC} ~ " ; grep -m 1 "model name" /proc/cpuinfo | sed 's/^[model name:.* \t]*//'
+
 # installed packages from package manager
 echo -ne "${CYAN}pkgs${NC} ~ " ; pacman -Q | wc -l
+
 # MemTotal in /proc/meminfo
 echo -ne "${BLUE}ram${NC} ~ " ; awk '/MemTotal:/ {printf "%d MiB\n", $2 / 1024}' /proc/meminfo 
-# TERM info from printenv
+
+# print $TERM variable
 echo -ne "${PURPLE}term${NC} ~ " ; echo $TERM
