9eeaba1d08c4e15abe233971702d15fdc9381a43
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-22 16:04:04 -0600

    make uptime print if uptime --pretty not found

--- fetch.sh
+++ fetch.sh
@@ -25,15 +25,15 @@ uname -r
 
 # // UPTIME // run 'uptime'
 echo -ne "${CYAN}uptime${NC} ~ "
-# Check architecture - busybox version doesn't support pretty flag
-if [ `readlink -f /usr/bin/uptime` != "/bin/busybox" ] ; then
-   uptime --pretty | sed -e 's/up//;s/^ *//'
+# check for uptime --pretty instead of file; run if found
+if [[ $(command -v uptime --pretty )  ]] ; then
+	uptime --pretty | sed -e 's/up//;s/^ *//'
 else
-   UPTIME=$(uptime)
-   UPTIME=$(echo -n "$UPTIME" | sed -e 's/^.*up //')
-   UPTIME=$(echo "$UPTIME" | sed -e 's/load.*$//')
-   UPTIME=${UPTIME%\,*}
-   echo "$UPTIME"
+	UPTIME=$(uptime)
+	UPTIME=$(echo -n "$UPTIME" | sed -e 's/^.*up //')
+	UPTIME=$(echo "$UPTIME" | sed -e 's/load.*$//')
+	UPTIME=${UPTIME%\,*}
+	echo "$UPTIME"
 fi
 
 
