cfd04f7c9166ccc45fa1621c31fb95dac86dad8a
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-22 22:19:57 -0600

    add support for multiple swapfiles or swap partitions

--- fetch.sh
+++ fetch.sh
@@ -135,13 +135,21 @@ awk '/MemTotal:/ {printf "%d MiB\n", $2 / 1024}' /proc/meminfo | tr -d '\n'
 
 
 # // SWAP // print 'Size' from /proc/swaps
-swap_kb=$(head /proc/swaps | grep '/dev' | awk '{print($3)}')
 swap_count=$(head /proc/swaps | wc -l)
-if [[ $swap_count -ge 2 ]] ; then
-	let "swap_mb = $swap_kb / 1024"
-	echo -ee " \e \e \e \e " | tr -d '\n'
-	echo -ne "${YELLOW}swap${NC} ~ "
-	echo -e "$swap_mb MiB"
+swap1_kb=$(sed -n '2p' /proc/swaps | awk '{print($3)}')
+swap2_kb=$(sed -n '3p' /proc/swaps | awk '{print($3)}')
+if [[ $swap_count = 2 ]] ; then  
+        let "swap1_mb = $swap1_kb / 1024"
+        echo -e " \e \e \e \e " | tr -d '\n'
+        echo -ne "${YELLOW}swap${NC} ~ "
+        echo -e "$swap1_mb MiB"     
+elif [[ $swap_count -ge 3 ]] ; then 
+        let "swap1_mb = $swap1_kb / 1024"
+        echo -e " \e \e \e \e " | tr -d '\n'
+        echo -ne "${YELLOW}swap${NC} ~ "
+        echo -ne "$swap1_mb MiB " | sed 's/ //'
+        let "swap2_mb = $swap2_kb / 1024"
+        echo -e "$swap2_mb MiB" | sed 's/ //'
 fi
 
 
