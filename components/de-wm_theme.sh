433c555940c40b7e0b0d0291e6ff1b14b9e5110c
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-23 19:32:44 -0600

    #18 solution

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -2,12 +2,12 @@
 
 # // DE/WM // if file exist print 'DesktopNames'
 echo -ne "${YELLOW}de/wm${NC} ~ "
-if test -e /usr/share/xsessions/  ; then
+if test -e /usr/share/xsession/  ; then
          cat /usr/share/xsessions/* | grep -i 'names=' | sed 's/DesktopNames=//' | tr -d '\n'
 elif test -e /usr/share/wayland-sessions/* ; then
         cat /usr/share/wayland-sessions/* | grep -i 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
-elif [[ $(command -v xprop) ]] ; then
-        xprop -root | grep '^_NET_WM_NAME' | sed 's/^_NET_WM_NAME//g;s/(UTF8_STRING) = //g' | tr -d '\n'
+elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
+        xprop -root | grep '^_NET_WM_NAME' | sed 's/^_NET_WM_NAME//g;s/(UTF8_STRING) = //g' | tr -d '"\n'
 else
         echo not found
 fi
