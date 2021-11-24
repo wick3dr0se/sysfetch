fc572df2eda44315f285a392d34bb89c2b72062f
Khiem Ton That <29520244+th4tkh13m@users.noreply.github.com>
2021-11-24 09:57:48 +0000

    Fix xprop

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -7,8 +7,9 @@ if [[ -e /usr/share/xsessions/ ]] ; then
 elif [[ -e /usr/share/wayland-sessions/ ]] ; then
         head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
-        xprop -root | grep -m1 '^_NET_WM_NAME' | sed 's/^_NET_WM_NAME//g;s/(UTF8_STRING) = //g' | tr -d '"\n'
-else
+	id=$(xprop -root | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
+	echo $(xprop -id "$id" | sed -n '/^_NET_WM_NAME/ s/.* // p' | sed 's/"//g') | tr -d "\n"
+
         echo not found
 fi
 
