2ad68d35ffda4c7f95ebe3843cb0b49b39eb5b78
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-23 20:02:05 -0600

    fix theme repeat; output not found if gsettings variable returns null #37

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -3,11 +3,11 @@
 # // DE/WM // if file exist print 'DesktopNames'
 echo -ne "${YELLOW}de/wm${NC} ~ "
 if test -e /usr/share/xsession/  ; then
-         cat /usr/share/xsessions/* | grep -i 'names=' | sed 's/DesktopNames=//' | tr -d '\n'
-elif test -e /usr/share/wayland-sessions/* ; then
-        cat /usr/share/wayland-sessions/* | grep -i 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
+         head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//' | tr -d '\n'
+elif test -e /usr/share/wayland-sessions/ ; then
+        head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
-        xprop -root | grep '^_NET_WM_NAME' | sed 's/^_NET_WM_NAME//g;s/(UTF8_STRING) = //g' | tr -d '"\n'
+        xprop -root | grep -m1 '^_NET_WM_NAME' | sed 's/^_NET_WM_NAME//g;s/(UTF8_STRING) = //g' | tr -d '"\n'
 else
         echo not found
 fi
@@ -18,10 +18,11 @@ fi
 echo -ne " \e \e \e \e "
 echo -ne "${BLUE}theme${NC} ~ "
 gtk_name=$(grep 'gtk-theme-name' ~/.config/gtk-3.0/settings.ini | sed 's/gtk-theme-name=//g' | sed 's/-/ /g')
+theme_name=$(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g" | tr -d '\n')
 if [[ ! -z "$gtk_name" ]] ; then
 	echo $gtk_name
-elif [[ $(command -v gsettings) ]] ; then
-	echo $(gsettings get org.gnome.desktop.interface gtk-theme | sed "s/'//g" | tr -d '\n')
+elif [[ ! -z "$theme_name" ]] ; then
+	echo $theme_name
 else
 	echo not found
 fi
