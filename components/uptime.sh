4c83a1fb17bd6c57b9c4b2c690f0f671408fb756
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-24 15:42:16 -0600

    fix uptime per #46

--- components/uptime.sh
+++ components/uptime.sh
@@ -4,11 +4,11 @@
 # // UPTIME // run 'uptime'
 echo -ne "${CYAN}uptime${NC} ~ "
 # if /proc/uptime found convert time
-raw_sec=$(head /proc/uptime | awk '{print $1}')
-raw_sec=${raw_sec%\.*}
+sec=$(head /proc/uptime | awk '{print $1}')
+sec=${sec%\.*}
 if [[ -e /proc/uptime ]] ; then
 	#da math
-	printf '%d weeks, %d hours, %d minutes \n' $(($raw_sec/604800)) $(($raw_sec/3600)) $(($raw_sec%3600/60))
+	printf '%d days, %d hours, %d minutes \n' $(($sec%604800/86400)) $(($sec%86400/3600)) $(($sec%3600/60))
 else
 	echo timeless
 fi
