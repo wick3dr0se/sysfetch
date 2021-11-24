5d70c5ca54ecf0611b1c5ec8ca26485d24585c88
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-24 15:13:53 -0600

    strip :-; from de/wm

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -3,7 +3,7 @@
 # // DE/WM // if file exist print 'DesktopNames'
 echo -ne "${YELLOW}de/wm${NC} ~ "
 if [[ -e /usr/share/xsessions/ ]] ; then
-         head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//;s/CLASSIC//;s/Ubuntu//;s/ubuntu//;s/Classic//;s/GNOME//2g' | tr -d '\n'
+         head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//;s/CLASSIC//;s/Ubuntu//;s/ubuntu//;s/Classic//;s/GNOME//2g' | tr -d ':-;\n'
 elif [[ -e /usr/share/wayland-sessions/ ]] ; then
         head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
-	id=$(xprop -root | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
-	echo $(xprop -id "$id" | sed -n '/^_NET_WM_NAME/ s/.* // p' | sed 's/"//g') | tr -d "\n"
-fi
