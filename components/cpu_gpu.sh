ffde95a01464da4e42491810edbf65392a146eb0
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-24 16:28:20 -0600

    change cpufreq

--- components/cpu_gpu.sh
+++ components/cpu_gpu.sh
@@ -14,10 +14,10 @@ fi
 
 # get cpu frequency if /sys/devices/system/cpu exist
 if [[ -e /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq ]] ; then
-        max_cpu=$(head /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | sed 's/......$/.&/;s/....$//' | tr -d '\n')
-        scal_cpu=$(head /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq | sed 's/......$/.&/;s/.....$//' | tr -d '\n')
+        max_cpu=$(head /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq | sed 's/......$/.&/;s/.....$//' | tr -d '\n')
+        cur_cpu=$(head /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq | sed 's/......$/.&/;s/....$//' | tr -d '\n')
         echo -ne "${CYAN}$max_cpu${NC}"
-        echo -ne "@${YELLOW}$scal_cpu${NC}" ; echo GHz
+        echo -ne "@${YELLOW}$cur_cpu${NC}" ; echo GHz
 else
         echo -ne "\n"
 fi
