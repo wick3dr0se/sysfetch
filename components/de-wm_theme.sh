48dd6db9b4c9d742c7501cc0be22243a60aa75bb
Khiem Ton That <29520244+th4tkh13m@users.noreply.github.com>
2021-11-24 10:01:02 +0000

    Update de-wm_theme.sh

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -9,7 +9,7 @@ elif [[ -e /usr/share/wayland-sessions/ ]] ; then
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
 	id=$(xprop -root | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
 	echo $(xprop -id "$id" | sed -n '/^_NET_WM_NAME/ s/.* // p' | sed 's/"//g') | tr -d "\n"
-
+else
         echo not found
 fi
 
