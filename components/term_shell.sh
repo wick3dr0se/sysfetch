33c45df22a700c3c53366105f5204e1cc4f39c4c
wick3dr0se <56238258+wick3dr0se@users.noreply.github.com>
2021-11-25 03:39:17 -0600

    Send pstree output to null

--- components/term_shell.sh
+++ components/term_shell.sh
@@ -5,7 +5,7 @@
 init_strip="s/init//g;s/systemd//g"
 shell_strip="s/fish//g;s/bash//g;s/zsh//g;s/ash//g"
 shell=$(echo "$SHELL" | sed 's%.*/%%')
-term=$(pstree -sA $$ | head -n1 | sed "s/head//g;s/fetch.sh//g;$init_strip;$shell_strip;s/^-*//;s/+//;s/-*$//")
+term=$(pstree -sA $$ 2>/dev/null | head -n1 | sed "s/head//g;s/fetch.sh//g;$init_strip;$shell_strip;s/^-*//;s/+//;s/-*$//")
 if [[ ! -z $shell ]] ; then
 	echo -ne "${YELLOW}term${NC} ~ "
 	echo -ne "$term"
