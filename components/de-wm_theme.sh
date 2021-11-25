cb564383cd81107f8a32891a7c5b83f453c6371d
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-24 23:47:20 -0600

    fix #39 run XDG_CURRENT_DESKTOP if exist

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # // DE/WM // if file exist print 'DesktopNames'
+session=$(echo $XDG_CURRENT_DESKTOP)
 echo -ne "${YELLOW}de/wm${NC} ~ "
 if [[ -e /usr/share/xsessions/ ]] ; then
          head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//;s/CLASSIC//;s/Ubuntu//;s/ubuntu//;s/Classic//;s/GNOME//2g' | tr -d ':-;\n'
+elif [[ ! -z "$session" ]] ; then
+	echo -ne "$session"
 elif [[ -e /usr/share/wayland-sessions/ ]] ; then
         head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
 	id=$(xprop -root | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
 	echo $(xprop -id "$id" | sed -n '/^_NET_WM_NAME/ s/.* // p' | sed 's/"//g') | tr -d "\n"
 else
-        echo -n not found
+        echo -n "not found"
 fi
 
 
@@ -20,11 +23,11 @@ echo -ne "${BLUE}theme${NC} ~ "
 gtk_name=$(grep 'gtk-theme-name' ~/.config/gtk-3.0/settings.ini 2>/dev/null | sed 's/gtk-theme-name=//g' | sed 's/-/ /g')
 theme_name=$(gsettings get org.gnome.desktop.interface gtk-theme 2>/dev/null | sed "s/'//g" | tr -d '\n')
 if [[ ! -z "$gtk_name" ]] ; then
-  echo "$gtk_name"
+	echo "$gtk_name"
 elif [[ ! -z "theme_name" ]] ; then
-  echo "$theme_name"
+	echo "$theme_name"
 else
-  echo "not found"
+	echo "not found"
 fi
 
 
