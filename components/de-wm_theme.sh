0d396b83993714c11ea5f5ca9ce73c0211a79de0
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-25 20:58:48 -0600

    direct xprop to null; hide theme row if not found #15

--- components/de-wm_theme.sh
+++ components/de-wm_theme.sh
@@ -2,16 +2,19 @@
 
 # // DE/WM // if file exist print 'DesktopNames'
 session=$(echo $XDG_CURRENT_DESKTOP)
-echo -ne "${YELLOW}de/wm${NC} ~ "
-
+dewm="${YELLOW}de/wm${NC} ~ "
 if [[ ! -z "$session" ]] ; then
+	echo -ne "$dewm"
 	echo -ne "$session"
 elif [[ -e /usr/share/xsessions/ ]] ; then
-         head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//;s/CLASSIC//;s/Ubuntu//;s/ubuntu//;s/Classic//;s/GNOME//2g' | tr -d ':-;\n'
+        echo -ne "$dewm"
+	head /usr/share/xsessions/* | grep -im1 'names=' | sed 's/DesktopNames=//;s/CLASSIC//;s/Ubuntu//;s/ubuntu//;s/Classic//;s/GNOME//2g' | tr -d ':-;\n'
 elif [[ -e /usr/share/wayland-sessions/ ]] ; then
-        head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
+        echo -ne "$dewm"
+	head /usr/share/wayland-sessions/* | grep -im1 'name=' | sed 's/name=//gi' | sort -u | sed ':a;N;$!ba;s/\n/, /gi' | tr -d '\n'
 elif [[ $(command -v xprop) ]] ; [[ ! -z $DISPLAY ]] ; then
-	id=$(xprop -root | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
+	id=$(xprop -root 2>/dev/null | sed -n '/^_NET_SUPPORTING_WM_CHECK/ s/.* // p')
+	echo -ne "$dewm"
 	echo $(xprop -id "$id" | sed -n '/^_NET_WM_NAME/ s/.* // p' | sed 's/"//g') | tr -d "\n"
 else
         echo -n "not found"
