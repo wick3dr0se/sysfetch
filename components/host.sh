3c997c994b1f7ac599359285ecc74f753f9ddb09
wick3dr0se <wick3dr0se@protonmail.com>
2021-11-28 18:26:06 -0600

    merge alpha to master

--- components/host.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# // HOST@USER // w/ uname & $USER variable
-echo -ne "\e[3m${PURPLE}$user${NC}\e[0m"
-echo -e "\e[3m@${BLUE}$host${NC}\e[0m"
--- components/load.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# // LOAD AVGS // w shell redirect into awk
-echo -ne "${PURPLE}load${NC} ~ "
-head /proc/loadavg
