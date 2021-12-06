c551cad828f36e0e77dd84b71fcc365a9ac73af9
wick3dr0se <wick3dr0se@protonmail.com>
2021-12-06 17:27:55 -0600

    fix btw version printing without Arch Linux; get distro variable without version so ascii text prints

--- components/distro_arch.sh
+++ components/distro_arch.sh
@@ -2,9 +2,10 @@
 # // DISTRO //
 if [[ "$os" = "Darwin" ]] ; then
 	echo -ne "${CYAN}distro${NC} ~ $(sw_vers -productVersion) "
-elif [[ ! -z  $distro ]] ; then
-	[[ "$distro" = "Arch Linux" ]] ; btw="-btw"
-	echo -ne "${CYAN}distro${NC} ~ $distro$btw \e \e \e \e "
+elif [[ "$distro" = "Arch Linux" ]] ; then
+	echo -ne "${CYAN}distro${NC} ~ $distro (btw) \e \e \e \e "
+elif [[ ! -z  "$distro" ]] ; then
+	echo -ne "${CYAN}distro${NC} ~ $distro \e \e \e \e "
 else
 	echo -ne "${CYAN}distro${NC} ~ not found \e \e \e \e "
 fi
