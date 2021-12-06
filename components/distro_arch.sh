4d1c5773b96087eeb441576203270788f7f119f5
wick3dr0se <wick3dr0se@protonmail.com>
2021-12-05 23:13:22 -0600

    add ascii text per distro

--- components/distro_arch.sh
+++ components/distro_arch.sh
@@ -3,8 +3,8 @@
 if [[ "$os" = "Darwin" ]] ; then
 	echo -ne "${CYAN}distro${NC} ~ $(sw_vers -productVersion) "
 elif [[ ! -z  $distro ]] ; then
-	[[ "$distro" = "Arch Linux" ]] ; btw="(btw)"
-	echo -ne "${CYAN}distro${NC} ~ $distro $btw \e \e \e \e "
+	[[ "$distro" = "Arch Linux" ]] ; btw="-btw"
+	echo -ne "${CYAN}distro${NC} ~ $distro$btw \e \e \e \e "
 else
 	echo -ne "${CYAN}distro${NC} ~ not found \e \e \e \e "
 fi
