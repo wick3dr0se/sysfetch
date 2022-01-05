e6378981c89d70ab8f2e69efe995b48658382b95
wick3dr0se <wick3dr0se@protonmail.com>
2022-01-05 17:38:36 -0600

    refractor && rmv hooks

--- sys/linux.sh
+++ sys/linux.sh
@@ -1,204 +1,238 @@
 #/bin/bash
 
-# get hooks
-[[ -e sysfetch ]] && source "assets/hooks.sh" || source "/usr/share/sysfetch/assets/hooks.sh"
+set -e
 
 # /USER/ get username from $USER environment variable or from id command
-user=${USER:-$(id -un)}
+_user() {
+user="${USER:-$(id -un)}"
+}
 
 # /HOST/ get hostname from $HOSTNAME or $hostname environment variables then uname
-hostname=${HOSTNAME:-$hostname:-$host}
+_host() {
+hostname="${HOSTNAME:-$hostname:-$host}"
+}
 
 # /UPTIME/ convert raw seconds from /proc/uptime
-d="/proc/uptime"
-if dir $d ; then
-	read -r sec < $d
-	sec=${sec//.*}
-	sec=${sec%\.*}
-	days=$((sec/86400))
-	hrs=$((sec%86400/3600))
-	mins=$((sec%3600/60))
-	if is $days 0 ; is $hrs "0" ; then
-		uptime="$mins mins"
-	elif is $days 0 ; then
-		uptime="$hrs hrs, $mins mins"
+_uptime() {
+p='/proc/uptime'
+if [[ -f "${p}" ]] ; then
+	read sec < "${p}"
+	sec="${sec//.*}"
+	sec="${sec%\.*}"
+	days="$((sec/86400))"
+	hrs="$((sec%86400/3600))"
+	mins="$((sec%3600/60))"
+	if [[ "$days" = '0' && "${hrs}" = '0' ]] ; then
+		uptime="${mins} mins"
+	elif [[ "${days}" = '0' ]] ; then
+		uptime="${hrs} hrs, ${mins} mins"
 	else
-		uptime="$days days, $hrs hrs, $mins mins"
+		uptime="${days} days, ${hrs} hrs, ${mins} mins"
 	fi
-elif comm uptime ; then
-	uptime=$(uptime | awk '{print $3}')
-	uptime=${uptime//,}
+elif [[ $(command -v uptime) ]] ; then
+	uptime="$(uptime | awk '{print $3}')"
+	uptime="${uptime//,}"
 fi
+}
 
 # /KERNEL/ get kernel from uname or /proc/version
-kernel_rel=${kernel_rel:-$(awk '{print $3}' /proc/version)}
+_kernel() {
+kernel_rel="${kernel_rel:-$(awk '{print $3}' '/proc/version')}"
+}
 
 # /DISTRO/ check os-release for distrobution
-d="/etc/os-release" ;dir $d && read -r distro < $d ||
-d="/etc/issue" ; dir $d && read -r line < $d ; distro=${line%%\\*}
-	
-distro=${distro//NAME=}
-distro=${distro//'"'}
-is $distro *Arch* && distro="$distro (btw)"
+_distro()  {
+if [[ -f '/etc/os-release' ]] ; then
+	read distro < '/etc/os-release'
+elif [[ -f '/etc/issue' ]] ; then
+	read distro < '/etc/issue'
+	distro="${distro// r*}"	
+fi
+distro="${distro//NAME=}"
+distro="${distro//\"}"
+[[ "${distro}" = *'Arch'* ]] && distro="${distro} (btw)"
+}
 
 # /ARCH/ 
 # taken from uname
 
 # /TERM/ get terminal from 2nd field of pstree output (need new method)
-while read -r line ; do
-	case $line in
-		PPid*) ppid=${line#PPid:} ;;
-	esac
-done < /proc/$PPID/status
-
-while read -r line ; do
-	case $line in
-		*:*) term=${line##* } ;;
-	esac
-done < <(ps -f $ppid)
+_term() {
+p="/proc/${PPID}/status"
+if [[ -f "${p}" ]] ; then
+	while read line ; do
+		case "${line}" in
+			'PPid'*) line="${line#PPid:}" ; ppid="$(xargs <<< ${line})" ;;
+		esac
+	done < "${p}"
+fi
+if [[ $(command -v ps) ]] ; then
+	while read line ; do
+		case "${line}" in
+			*':'*) term="${line##* }" ;;
+		esac
+	done < <(ps -f "${ppid}")
+fi
+}
 
 # /SHELL/ check shell environment variable
-shell=${SHELL##*/}
+_shell() {
+shell="${SHELL##*/}"
+}
 
 # /DE/WM/ get desktop environment or window manager
-if var $XDG_CURRENT_DESKTOP ; then
-	de_wm=$XDG_CURRENT_DESKTOP
-elif var $DESKTOP_SESSION ; then
-	de_wm=$DESKTOP_SESSION
-elif comm wmctrl ; then
-	while read -r line ; do
-		case $line in
-			Name:*) de_wm=${line#Name: } ;;
+_de_wm() {
+if [[ ! -z "${XDG_CURRENT_DESKTOP}" ]] ; then
+	de_wm="${XDG_CURRENT_DESKTOP}"
+elif [[ ! -z "${DESKTOP_SESSION}" ]] ; then
+	de_wm="${DESKTOP_SESSION}"
+elif [[ $(command -v wmctrl) ]] ; then
+	while read line ; do
+		case "${line}" in
+			'Name:'*) de_wm="${line#Name: }" ;;
 		esac
 	done < <(wmctrl -m)
-elif comm xprop ; then
-	while read -r line ; do
-		case $line in
-			_NET_WM_NAME*) line=${line##*=} ; de_wm=${line//\"} ;;
+elif [[ $(command -v xprop) ]] ; then
+	while read line ; do
+		case "${line}" in
+			'_NET_WM_NAME'*) line="${line##*=}" ; de_wm="${line//\"}" ;;
 		esac
 	done < <(xprop -root)
 fi
+}
 
 # /THEME/ stat theme {needs more methods}
-d=".config/gtk-3.0/settings.ini"
-if dir ~/$d ; then
-	while read -r line ; do
-		case $line in
-			gtk-theme*) theme=$line ;;
+_theme() {
+p='.config/gtk-3.0/settings.ini'
+if [[ -f "~/${p}" ]] ; then
+	while read line ; do
+		case "${line}" in
+			gtk-theme*) theme="${line}" ;;
 			### maybe we should output icons?
-			gtk-icon*) icons=$line ;;
+			gtk-icon*) icons="${line}" ;;
 		esac
-	done < ~/$d 
-elif comm gsettings ; then
-	theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
-	theme=${theme//"'"}
+	done < "~/${p}"
+elif [[ $(command -v gsettings) ]] ; then
+	theme="$(gsettings get org.gnome.desktop.interface gtk-theme)"
+	theme="${theme//\'}"
 fi
-theme=${theme##*=}
-theme=${theme/-/ }
+theme="${theme##*=}"
+theme="${theme/-/ }"
+}
 
 # /PKGS/ return package count - try not to use package manager, it's slow
-if comm pacman ; then
-	pacman=$(ls /var/lib/pacman/local | wc -l)
-	src_pkgs=$(ls /usr/local/bin | wc -l)
-	pkgs="$pacman (pacman) $src_pkgs (src)"
-elif comm dpkg-query ; then
-	pkgs=$(dpkg-query -l | grep -c '^li')
-elif comm dnf ; then
-	pkgs=$(dnf list installed | grep ".@." -c)
-elif comm rpm ; then
-	pkgs=$(rpm -qa | wc -l)
-elif dir /var/log/packages ; then
-	pkgs=$(ls /var/log/packages | wc -l)
-elif comm opkg ; then
-	pkgs=$(opkg list-installed | wc -w)
-elif comm emrge ; then
-	pkgs=$(ls -d /var/db/pkg/*/* | wc -l)
-elif comm xbps-query ; then
-	pkgs=$(xbps-query -l | grep -c '^li')
-elif comm guix ; then
-	pkgs=$(guix package --list-installed | wc -l)
-elif comm nixos-rebuild ; then
-	pkgs=$(ls /run/current-system/sw/bin | wc -l)
+_pkgs() {
+if [[ -d '/var/lib/pacman/local' ]] ; then
+	pacman="$(ls '/var/lib/pacman/local' | wc -l)"
+	src_pkgs="$(ls '/usr/local/bin' | wc -l)"
+	pkgs="${pacman} (pacman) ${src_pkgs} (src)"
+elif [[ $(command -v dpkg-query) ]] ; then
+	pkgs="$(dpkg-query -l | grep -c '^li')"
+elif [[ $(command -v dnf)  ]] ; then
+	pkgs="$(dnf list installed | grep ".@." -c)"
+elif [[ $(command -v rpm) ]] ; then
+	pkgs="$(rpm -qa | wc -l)"
+elif [[ -d '/var/log/packages' ]] ; then
+	pkgs="$(ls '/var/log/packages' | wc -l)"
+elif [[ $(command -v opkg) ]] ; then
+	pkgs="$(opkg list-installed | wc -w)"
+elif [[ $(command -v emrge) ]] ; then
+	pkgs="$(ls -d '/var/db/pkg/*/*' | wc -l)"
+elif [[ $(command -v xbps-query) ]] ; then
+	pkgs="$(xbps-query -l | grep -c '^li')"
+elif [[ $(command -v guix) ]] ; then
+	pkgs="$(guix package --list-installed | wc -l)"
+elif [[ $(command -v nixos-rebuild) ]] ; then
+	pkgs="$(ls '/run/current-system/sw/bin' | wc -l)"
 fi
+}
 
 # /CPU/ get cpu vendor and frequency
-d="/proc/cpuinfo"
-cpu_strip="s/Processor//;s/CPU//;s/(TM)//;s/(R)//;s/@//;s/ *$//"
-while read -r line ; do
-	case $line in
-		vendor*) cpu_vendor=${line#*:} ;;
-		model*) if is $cpu_vendor GenuineIntel ; then
-			line=${line#*:} ; line=${line::7} ; cpu=$(sed "$cpu_strip" <<< $line) 
+_cpu() {
+cpu_strip='s/Processor//;s/CPU//;s/(TM)//;s/(R)//;s/@//;s/ *$//'
+while read line ; do
+	case "${line}" in
+		'vendor'*) cpu_vendor="${line#*:}" ;;
+		'model'*) if [[ "${cpu_vendor}" = 'GenuineIntel' ]] ; then
+				line="${line#*:}" ; line="${line::7}" ; cpu="$(sed "${cpu_strip}" <<< ${line})" 
 			else
-			line=${line#*:} ; cpu=$(sed "$cpu_strip" <<< $line)
+				line="${line#*:}" ; cpu="$(sed "$cpu_strip" <<< ${line})"
 			fi ;;
 	esac
-done < $d
+done < '/proc/cpuinfo'
+}
 
-d="/sys/devices/system/cpu/cpu0/cpufreq"
-if dir $d ; then
-read -r max_cpu < $d/scaling_max_freq
-read -r cur_cpu < $d/scaling_cur_freq
-max_cpu=${max_cpu::-5}
-max_cpu=$(sed 's/.$/.&/' <<< $max_cpu)
-cur_cpu=${cur_cpu::-4}
-cur_cpu=$(sed 's/..$/.&/' <<< $cur_cpu)
-fi
+_cpu_freq() {
+# get max cpu frequency
+p='/sys/devices/system/cpu/cpu0/cpufreq'
+read max_cpu < "${p}/scaling_max_freq"
+max_cpu="${max_cpu::-5}"
+max_cpu="$(sed 's/.$/.&/' <<< ${max_cpu})"
+
+# get current cpu frequency
+read cur_cpu < "${p}/scaling_cur_freq"
+cur_cpu="${cur_cpu::-4}"
+cur_cpu="$(sed 's/..$/.&/' <<< ${cur_cpu})"
+}
 
 # /GPU/ strip common prefixes from output of lspci
-gpu_strip="s/Advanced Micro Devices, Inc. //;s/NVIDIA//;s/Corporation//;s/Controller//;s/controller//;s/storage//;s/filesystem//;s/Family//;s/Processor//;s/Mixture//;s/Model//;s/Generation/Gen/;s/^ *//"
-comm lspci &&
-while read -r line ; do
-	case $line in
-		*VGA*) line=$(tr -d '[]' <<< ${line##*:}) ; gpu=$(sed "$gpu_strip" <<< $line) ;;
-		*3D*) line=$(tr -d '[]' <<< ${line##*:}) ; gpu=$(sed "$gpu_strip" <<< $line) ;;
+_gpu() {
+gpu_strip='s/Advanced Micro Devices, Inc. //;s/NVIDIA//;s/Corporation//;s/Controller//;s/controller//;s/storage//;s/filesystem//;s/Family//;s/Processor//;s/Mixture//;s/Model//;s/Generation/Gen/;s/^ *//'
+while read line ; do
+	case "${line}" in
+		*'VGA'*) line="$(tr -d '[]' <<< ${line##*:})" ; gpu="$(sed "${gpu_strip}" <<< ${line})" ;;
+		*'3D'*) line="$(tr -d '[]' <<< ${line##*:})" ; gpu="$(sed "${gpu_strip}" <<< ${line})" ;;
 	esac
-done < <(lspci) 
+done < <(lspci)
+}
 
 # /MOBO/ return motherboard vendor + name
-d="/sys/devices/virtual/dmi/id"
-mobo_strip="s/COMPUTER INC.//"
-if dir $d ; then
-	read -r mobo_vendor < $d/board_vendor
-	read -r mobo_name < $d/board_name
-	mobo="$mobo_vendor $mobo_name"
-	mobo=$(sed "$mobo_strip" <<< $mobo)
-fi
+_mobo() {
+p='/sys/devices/virtual/dmi/id'
+mobo_strip='s/COMPUTER INC.//'
+read mobo_vendor < "${p}/board_vendor"
+read mobo_name < "${p}/board_name"
+mobo="${mobo_vendor} ${mobo_name}"
+mobo="$(sed "${mobo_strip}" <<< ${mobo})"
+}
 
 # /DISK/ return device name, root partition size and output disk usage
-comm df &&
-while read -r line ; do
-	case $line in
-		*) dis=${line#*G }
-			cur_disk=${dis%%G *}
-			dis=${line%%G*}
-			max_disk=${dis##* }
-			dis=${line##*G}
-			disk_per=${dis% *}
+_disk() {
+while read line ; do
+	case "${line}" in
+		'/'*) dis="${line#*G }"
+			cur_disk="${dis%%G *}"
+			dis="${line%%G*}"
+			max_disk="${dis##* }"
+			dis="${line##*G}"
+			disk_per="${dis% *}"
 			;;
-		*nvme*|*mmcblk*|*loop*) root=$(sed 's/p[0-9]*$//' <<< $line) ;;
-		*sd*|*vd*|*sr*) root=${line%% *} ; root=$(sed 's/[0-9]*$//' <<< $line) ;;
+		*'sd'*|*'vd'*|*'sr'*) line="${line%% *}" ; root="$(sed 's/[0-9]*$//' <<< ${line})" ;;
+		*'nvme'*|*'mmcblk'*) line="${line%% *}" ; root="$(sed 's/p[0-9]*$//' <<< ${line})" ;;
 	esac
 done < <(df -h /)
-disk_strip="s/ SSD//;s/ [0-9]*GB$//"
-comm lsblk && disk_model=$(lsblk -n $root -io MODEL | sed "$disk_strip" | head -n1)
-
+disk_strip='s/ SSD//;s/ [0-9]*GB$//'
+[[ $(command -v lsblk) ]] && disk_model="$(lsblk ${root} -no 'MODEL' | sed "${disk_strip}" | head -n1)"
+}
 
 # /RAM/ get memory kb from meminfo
-while read -r line ; do
-	case $line in
-		Active:*) v=${line#*:} ; v=${v//kB} ; v=${v//.*} ; cur_ram=$(($v/1024)) ;;
-		MemTot*) v=${line#*:} ; v=${v//kB} ; v=${v//.*} ; max_ram=$(($v/1024)) ;;
+_ram() {
+p='/proc/meminfo'
+while read line ; do
+	case "${line}" in
+		'Active:'*) v="${line#*:}" ; v="${v//kB}" ; v="${v//.*}" ; cur_ram="$((${v}/1024))" ;;
+		'MemTot'*) v="${line#*:}" ; v="${v//kB}" ; v="${v//.*}" ; max_ram="$((${v}/1024))" ;;
 	esac
-done < /proc/meminfo
+done < "${p}"
+}
 
 # /SWAP/ combine two swaps into one
-d="/proc/swaps"
-if dir $d ; then
-cur_swap=$(awk 'FNR==2 {print $4/1024}' $d)
-cur_swap2=$(awk 'FNR==3 {print $4/1024}' $d)
-cur_swap=$(awk "BEGIN {print ${cur_swap1:-0}+${cur_swap2:-0}}")
-max_swap=$(awk 'FNR==2 {print $3/1024}' $d)
-max_swap2=$(awk 'FNR==3 {print $3/1024}' $d)
-max_swap=$(awk "BEGIN {print ${max_swap:-0}+${max_swap2:-0}}")
-fi
+_swap() {
+p='/proc/swaps'
+cur_swap="$(awk 'FNR==2 {print $4/1024}' "${p}")"
+cur_swap2="$(awk 'FNR==3 {print $4/1024}' "${p}")"
+cur_swap="$(awk "BEGIN {print "${cur_swap:-0}+${cur_swap2:-0}"}")" 
+max_swap="$(awk 'FNR==2 {print $3/1024}' "${p}")"
+max_swap2="$(awk 'FNR==3 {print $3/1024}' "${p}")"
+max_swap="$(awk "BEGIN {print "${max_swap:-0}+${max_swap2:-0}"}")"
+}
