6f70561817e195ea754a7806c8efc4396e26b664
SimoneSalzano <salzanosimone99@gmail.com>
2022-01-10 18:11:45 +0100

    Wrap wsl.sh stats fetching in proper functions

--- sys/wsl.sh
+++ sys/wsl.sh
@@ -1,161 +1,215 @@
 #!/bin/bash
 
-# get hooks
-[[ -e sysfetch ]] && source "assets/hooks.sh" || source "/usr/share/sysfetch/assets/hooks.sh"
-
-# /USER@HOST/
-user="$USER"
+# /USER/ get username from $USER environment variable or from id command
+_user() {
+	user="${USER:-$(id -un)}"
+}
 
 # /HOST/ get hostname from $HOSTNAME or $hostname environment variables then uname
-hostname=${HOSTNAME:-$hostname:-$host}
+_host() {
+	hostname="${HOSTNAME:-$hostname:-$host}"
+}
 
-# UPTIME /
-d="/proc/uptime"
-if dir $d ; then
-	read -r sec < $d
-	sec=${sec//.*}
-	sec=${sec%\.*}
-	days=$((sec/86400))
-	hrs=$((sec%86400/3600))
-	mins=$((sec%3600/60))
-	if is "$days" "0" ; is $hrs "0" ; then
-		uptime="$mins mins"
-	elif is "$days" "0" ; then
-		uptime="$hrs hrs, $mins mins"
-	else
-		uptime="$days days, $hrs hrs, $mins mins"
+# /UPTIME/ convert raw seconds from /proc/uptime
+_uptime() {
+	p='/proc/uptime'
+	if [[ -f "${p}" ]]; then
+		read sec <"${p}"
+		sec="${sec//.*/}"
+		sec="${sec%\.*}"
+		days="$((sec / 86400))"
+		hrs="$((sec % 86400 / 3600))"
+		mins="$((sec % 3600 / 60))"
+		if [[ "$days" = '0' && "${hrs}" = '0' ]]; then
+			uptime="${mins} mins"
+		elif [[ "${days}" = '0' ]]; then
+			uptime="${hrs} hrs, ${mins} mins"
+		else
+			uptime="${days} days, ${hrs} hrs, ${mins} mins"
+		fi
+	elif [[ $(command -v uptime) ]]; then
+		uptime="$(uptime | awk '{print $3}')"
+		uptime="${uptime//,/}"
 	fi
-elif comm uptime ; then
-	uptime=$(uptime | awk '{print $3}' | tr -d ',')
-fi
+}
 
-# /KERNEL/
-# taken from uname
+# /KERNEL/ get kernel from uname or /proc/version
+_kernel() {
+	kernel_rel="${kernel_rel:-$(awk '{print $3}' '/proc/version')}"
+}
 
 # /DISTRO/ check os-release for distrobution
-d="/etc/os-release"
-if dir $d ; then
-	read -r distro < $d
-elif dir /etc/issue ; then
-	distro=$(awk -F\\ '{print $1}' /etc/issue | sed 's/ $//')
-fi
-distro=${distro//NAME=}
-distro=${distro//'"'}
+_distro() {
+	if [[ -f '/etc/os-release' ]]; then
+		read distro <'/etc/os-release'
+	elif [[ -f '/etc/issue' ]]; then
+		read distro <'/etc/issue'
+		distro="${distro// r*/}"
+	fi
+	distro="${distro//NAME=/}"
+	distro="${distro//\"/}"
+	[[ "${distro}" = *'Arch'* ]] && distro="${distro} (btw)"
+}
 
 # /ARCH/
 # taken from uname
 
-# /TERM/
-comm pstree && term=$(pstree -sA $$ | awk -F--- '{print $3 ; exit}')
-term=${term/-/ }
+# /TERM/ get terminal from 2nd field of pstree output (need new method)
+_term() {
+	p="/proc/${PPID}/status"
+	if [[ -f "${p}" ]]; then
+		while read line; do
+			case "${line}" in
+			'PPid'*)
+				line="${line#PPid:}"
+				ppid="$(xargs <<<${line})"
+				;;
+			esac
+		done <"${p}"
+	fi
+	if [[ $(command -v ps) ]]; then
+		while read line; do
+			case "${line}" in
+			*':'*) term="${line##* }" ;;
+			esac
+		done < <(ps -f "${ppid}")
+	fi
+}
 
-# /SHELL/
-var $SHELL && shell=${SHELL##*/}
+# /SHELL/ check shell environment variable
+_shell() {
+	shell="${SHELL##*/}"
+}
 
-# /DE_WM/
-if var $XDG_CURRENT_DESKTOP ; then
-	de_wm=$XDG_CURRENT_DESKTOP
-elif var $DESKTOP_SESSION ; then
-	de_wm=$DESKTOP_SESSION
-elif comm wmctrl ; then
-	de_wm=$(wmctrl -m | awk -F ': ' '/Name/ {print $2}')
-elif dir /usr/share/xsession ; then
-	de_wm=$(awk -F= '/Name/ {print $2}' /usr/share/xsessions/* | tail -n1)
-elif dir /usr/share/wayland-session ; then
-	de_wm=$(awk -F= '/Name/ {print $2}' /usr/share/wayland-sessions/*)
-elif comm xprop ; then
-	id=$(xprop -root | awk -F '# ' '/WM_CHECK/ {print $2}')
-	de_wm=$(xprop -id $id 2>/dev/null | awk -F '"' '/WM_NAME/ {print $2}')
-fi
-# /THEME/
-d=".config/gtk-3.0/settings.ini"
-if dir ~/$d ; then
-	while read -r line ; do
-		case $line in
-			gtk-theme*) theme=$line ;;
+# /DE/WM/ get desktop environment or window manager
+_de_wm() {
+	if [[ ! -z "${XDG_CURRENT_DESKTOP}" ]]; then
+		de_wm="${XDG_CURRENT_DESKTOP}"
+	elif [[ ! -z "${DESKTOP_SESSION}" ]]; then
+		de_wm="${DESKTOP_SESSION}"
+	elif [[ $(command -v wmctrl) ]]; then
+		while read line; do
+			case "${line}" in
+			'Name:'*) de_wm="${line#Name: }" ;;
+			esac
+		done < <(wmctrl -m)
+	elif [[ $(command -v xprop) ]]; then
+		while read line; do
+			case "${line}" in
+			'_NET_WM_NAME'*)
+				line="${line##*=}"
+				de_wm="${line//\"/}"
+				;;
+			esac
+		done < <(xprop -root)
+	fi
+}
+
+# /THEME/ stat theme {needs more methods}
+_theme() {
+	p='.config/gtk-3.0/settings.ini'
+	if [[ -f "~/${p}" ]]; then
+		while read line; do
+			case "${line}" in
+			gtk-theme*) theme="${line}" ;;
 			### maybe we should output icons?
-			gtk-icon*) icons=$line ;;
-		esac
-	done < ~/$d
-elif comm gsettings ; then
-	theme=$(gsettings get org.gnome.desktop.interface gtk-theme | tr -d "'")
-fi
-theme=${theme##*=}
-theme=${theme/-/ }
+			gtk-icon*) icons="${line}" ;;
+			esac
+		done <"~/${p}"
+	elif [[ $(command -v gsettings) ]]; then
+		theme="$(gsettings get org.gnome.desktop.interface gtk-theme)"
+		theme="${theme//\'/}"
+	fi
+	theme="${theme##*=}"
+	theme="${theme/-/ }"
+}
 
-# /PKGS/ return package count
-if comm pacman ; then
-	pacman=$(pacman -Qn | wc -l)
-	aur=$(pacman -Qqm | wc -l)
-	compiled=$(ls /usr/local/bin | wc -l)
-	pkgs="$pacman (pacman) $aur (aur) $compiled (src)"
-elif comm dpkg-query ; then
-	pkgs="$(dpkg-query -l | wc -l)"
-elif comm dnf ; then
-	pkgs=$(dnf list installed | grep ".@." -c)
-elif comm rpm ; then
-	pkgs=$(rpm -qa | wc -l)
-elif dir /var/log/packages ; then
-	pkgs=$(ls /var/log/packages | wc -l)
-elif comm opkg ; then
-	pkgs=$(opkg list-installed | wc -w)
-elif comm emrge ; then
-	pkgs=$(ls -d /var/db/pkg/*/* | wc -l)
-elif comm xbps-query ; then
-	pkgs=$(xbps-query -l | grep -c '^li')
-elif comm guix ; then
-	pkgs=$(guix package --list-installed | wc -l)
-elif comm nixos-rebuild ; then
-	pkgs=$(ls /run/current-system/sw/bin | wc -l)
-fi
+# /PKGS/ return package count - try not to use package manager, it's slow
+_pkgs() {
+	if [[ -d '/var/lib/pacman/local' ]]; then
+		pacman="$(ls '/var/lib/pacman/local' | wc -l)"
+		src_pkgs="$(ls '/usr/local/bin' | wc -l)"
+		pkgs="${pacman} (pacman) ${src_pkgs} (src)"
+	elif [[ $(command -v dpkg-query) ]]; then
+		pkgs="$(dpkg-query -l | wc -l)"
+	elif [[ $(command -v dnf) ]]; then
+		pkgs="$(dnf list installed | grep ".@." -c)"
+	elif [[ $(command -v rpm) ]]; then
+		pkgs="$(rpm -qa | wc -l)"
+	elif [[ -d '/var/log/packages' ]]; then
+		pkgs="$(ls '/var/log/packages' | wc -l)"
+	elif [[ $(command -v opkg) ]]; then
+		pkgs="$(opkg list-installed | wc -w)"
+	elif [[ $(command -v emrge) ]]; then
+		pkgs="$(ls -d '/var/db/pkg/*/*' | wc -l)"
+	elif [[ $(command -v xbps-query) ]]; then
+		pkgs="$(xbps-query -l | grep -c '^li')"
+	elif [[ $(command -v guix) ]]; then
+		pkgs="$(guix package --list-installed | wc -l)"
+	elif [[ $(command -v nixos-rebuild) ]]; then
+		pkgs="$(ls '/run/current-system/sw/bin' | wc -l)"
+	fi
+}
 
 # /CPU/ get cpu vendor and frequency
-d="/proc/cpuinfo"
-dir $d && cpu_vendor=$(awk -F ': ' '/vendor/ {print $2 ; exit}' $d)
-cpu_strip="s/Processor//;s/CPU//;s/(TM)//;s/(R)//;s/@//;s/ *$//"
-if is $cpu_vendor "GenuineIntel" ; then
-	cpu=$(awk -F ': ' '/name/ {print $2 ; exit}' $d | sed "$cpu_strip;s/.......$//" | xargs)
-else
-	cpu=$(awk -F ': ' '/name/ {print $2 ; exit}' $d | sed "$cpu_strip" | xargs)
-fi
+_cpu() {
+	cpu_strip='s/Processor//;s/CPU//;s/(TM)//;s/(R)//;s/@//;s/ [0-9]*.[0-9]*GHz//;s/ *$//;'
+	while read line; do
+		case "${line}" in
+		'vendor'*) cpu_vendor="${line#*:}" ;;
+		'model'*) if [[ "${cpu_vendor}" = 'GenuineIntel' ]]; then
+			line="${line#*:}"
+			line="${line::7}"
+			cpu="$(sed "${cpu_strip}" <<<${line})"
+		else
+			line="${line#*:}"
+			cpu="$(sed "$cpu_strip" <<<${line})"
+		fi ;;
+		esac
+	done <'/proc/cpuinfo'
+}
 
-cur_cpu="$(awk -v freq="$(wmic.exe cpu get currentclockspeed | sed -n 2p)" 'BEGIN {printf "%.2f", freq/1000}')"
-max_cpu="$(awk -v freq="$(wmic.exe cpu get maxclockspeed | sed -n 2p)" 'BEGIN {printf "%.2f", freq/1000}')"
+_cpu_freq() {
+	cur_cpu="$(awk -v freq="$(wmic.exe cpu get currentclockspeed | sed -n 2p)" 'BEGIN {printf "%.2f", freq/1000}')"
+	max_cpu="$(awk -v freq="$(wmic.exe cpu get maxclockspeed | sed -n 2p)" 'BEGIN {printf "%.2f", freq/1000}')"
+}
 
 # /GPU/
-gpu="$(wmic.exe path win32_VideoController get name | sed -n 2p)"
+_gpu() {
+	gpu="$(wmic.exe path win32_VideoController get name | sed -n 2p)"
+}
 # /MOBO/
-mobo="$(wmic.exe baseboard get product,Manufacturer | sed -n 2p | awk -F ' ' '{print $1, $2}')"
+_mobo() {
+	mobo="$(wmic.exe baseboard get product,Manufacturer | sed -n 2p | awk -F ' ' '{print $1, $2}')"
+}
 # /DISK/ return device name, root partition size and output disk usage
-windir="$(cmd.exe /c echo %windir% 2> /dev/null)"
-disk_name="${windir%%\\*}"
-sizes="$(wmic.exe logicaldisk where "name='$disk_name'" get size, freespace | sed -n 2p)"
-max_disk="$(awk '{print $2/1024/1024/1024}' <<< $sizes)"
-cur_disk="$(awk '{print ($2-$1)/1024/1024/1024}' <<< $sizes)"
-disk_per="$(awk '{print ($2-$1)/$2*100}' <<< $sizes)"
+_disk() {
+	windir="$(cmd.exe /c echo %windir% 2>/dev/null)"
+	disk_name="${windir%%\\*}"
+	sizes="$(wmic.exe logicaldisk where "name='$disk_name'" get size, freespace | sed -n 2p)"
+	max_disk="$(awk '{print $2/1024/1024/1024}' <<<$sizes)"
+	cur_disk="$(awk '{print ($2-$1)/1024/1024/1024}' <<<$sizes)"
+	disk_per="$(awk '{print ($2-$1)/$2*100}' <<<$sizes)"
+}
 
 # /RAM/ get memory kb from meminfo
-d="/proc/meminfo"
-if dir $d ; then
-	while read -r line ; do
-		case $line in
-			Active:*) cur_ram=${line#*:} ;;
-			MemTot*) max_ram=${line#*:} ;;
-		esac
-	done < $d
-	cur_ram=${cur_ram::-2}
-	max_ram=${max_ram::-2}
-	cur_ram=$((cur_ram/1024))
-	max_ram=$((max_ram/1024))
-fi
+_ram() {
+p='/proc/meminfo'
+while read line ; do
+	case "${line}" in
+		'Active:'*) v="${line#*:}" ; v="${v//kB}" ; v="${v//.*}" ; cur_ram="$((${v}/1024))" ;;
+		'MemTot'*) v="${line#*:}" ; v="${v//kB}" ; v="${v//.*}" ; max_ram="$((${v}/1024))" ;;
+	esac
+done < "${p}"
+}
 
-# /SWAP/
-d="/proc/swaps"
-if dir $d ; then
-cur_swap=$(awk 'FNR==2 {print $4/1024}' $d)
-cur_swap2=$(awk 'FNR==3 {print $4/1024}' $d)
-cur_swap=$(awk "BEGIN {print ${cur_swap1:-0}+${cur_swap2:-0}}")
-max_swap=$(awk 'FNR==2 {print $3/1024}' $d)
-max_swap2=$(awk 'FNR==3 {print $3/1024}' $d)
-max_swap=$(awk "BEGIN {print ${max_swap:-0}+${max_swap2:-0}}")
-fi
+# /SWAP/ combine two swaps into one
+_swap() {
+p='/proc/swaps'
+cur_swap="$(awk 'FNR==2 {print $4/1024}' "${p}")"
+cur_swap2="$(awk 'FNR==3 {print $4/1024}' "${p}")"
+cur_swap="$(awk "BEGIN {print "${cur_swap:-0}+${cur_swap2:-0}"}")" 
+max_swap="$(awk 'FNR==2 {print $3/1024}' "${p}")"
+max_swap2="$(awk 'FNR==3 {print $3/1024}' "${p}")"
+max_swap="$(awk "BEGIN {print "${max_swap:-0}+${max_swap2:-0}"}")"
+}
